cea3e539fdc3d3c9e9994511d34db8517224eab4
Alex Kalmakov <st00nsa@gmail.com>
2019-12-02 21:21:11 +0200

    Second day - second star

--- 2/main.go
+++ 2/main.go
@@ -30,42 +30,58 @@ func main() {
 	check(err)
 	input := string(inputBytes)
 	strings := strings.Split(input, ",")
-	numbers := make([]int, len(strings))
+	initNumbers := make([]int, len(strings))
 	i := 0
 
 	// fmt.Print(input)
 
 	for i, s := range strings {
-		numbers[i], _ = strconv.Atoi(s)
+		initNumbers[i], _ = strconv.Atoi(s)
+	}
+	numbers := make([]int, len(initNumbers))
+
+	for x := 0; x < 100; x++ {
+		for y := 0; y < 100; y++ {
+			copy(numbers, initNumbers)
+			numbers[1] = x
+			numbers[2] = y
+			i = 0
+
+			for true {
+				if numbers[i] == 1 {
+					to := numbers[i+3]
+					from1 := numbers[i+1]
+					from2 := numbers[i+2]
+					numbers[to] = numbers[from1] + numbers[from2]
+					i += 4
+					continue
+				}
+
+				if numbers[i] == 2 {
+
+					to := numbers[i+3]
+					from1 := numbers[i+1]
+					from2 := numbers[i+2]
+					numbers[to] = numbers[from1] * numbers[from2]
+					i += 4
+					continue
+				}
+
+				if numbers[i] == 99 {
+					if numbers[0] == 19690720 {
+						fmt.Println(numbers)
+						fmt.Println(100*numbers[1] + numbers[2])
+					} else {
+						// fmt.Println(numbers[0])
+					}
+					break
+				}
+
+				panic("Strange op code: " + string(numbers[i]))
+			}
+		}
 	}
 
-	for true {
-		if numbers[i] == 1 {
-			to := numbers[i+3]
-			from1 := numbers[i+1]
-			from2 := numbers[i+2]
-			numbers[to] = numbers[from1] + numbers[from2]
-			i += 4
-			continue
-		}
-
-		if numbers[i] == 2 {
-
-			to := numbers[i+3]
-			from1 := numbers[i+1]
-			from2 := numbers[i+2]
-			numbers[to] = numbers[from1] * numbers[from2]
-			i += 4
-			continue
-		}
-
-		if numbers[i] == 99 {
-			break
-		}
-
-		panic("Strange op code: " + string(numbers[i]))
-	}
-
-	fmt.Println(numbers)
+	fmt.Println("end")
 
 }
