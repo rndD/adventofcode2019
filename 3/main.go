bd69e9b393b52b67331faf085aade66c2282a0e5
Alex Kalmakov <st00nsa@gmail.com>
2019-12-03 20:43:22 +0200

    silly day three - first start

--- 3/main.go
+++ 3/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -13,21 +14,75 @@ func check(e error) {
 	}
 }
 
+func print2D(a *[30000][30000]int) {
+	for _, line := range a {
+		fmt.Println(line)
+	}
+}
+
+func setLine(nLine int, a *[30000][30000]int, x int, y int) {
+	if a[x][y] < nLine {
+		a[x][y] += nLine
+	}
+}
+
+func findSmallestDist(a *[30000][30000]int, centerX int, centerY int) int {
+	smallestDistance := 0.0
+	for y, lineX := range a {
+		for x, value := range lineX {
+			if value == 3 {
+				dist := math.Sqrt(math.Pow(float64(x-centerX), 2)) + math.Sqrt(math.Pow(float64(y-centerY), 2))
+				if smallestDistance == 0 || dist < smallestDistance {
+					smallestDistance = dist
+				}
+			}
+		}
+	}
+	return int(smallestDistance)
+}
+
 func main() {
 	inputBytes, err := ioutil.ReadFile("./input.txt")
 	check(err)
 	input := string(inputBytes)
 	lines := strings.Split(input, "\n")
-	// len := 100
-	// middle := len / 2
-	// field := [100][100]uint8{}
+	len := 30000
+	middle := len / 2
+	field := [30000][30000]int{}
 
-	for _, line := range lines {
+	for nLine, line := range lines {
+		nLine++
 		commands := strings.Split(line, ",")
+		x := middle
+		y := middle
 		for _, command := range commands {
 			arg := command[:1]
 			count, _ := strconv.Atoi(command[1:])
-			fmt.Println(arg, count)
+			if arg == "R" {
+				for i := 0; i < count; i++ {
+					x++
+					setLine(nLine, &field, x, y)
+				}
+			}
+			if arg == "L" {
+				for i := 0; i < count; i++ {
+					x--
+					setLine(nLine, &field, x, y)
+				}
+			}
+			if arg == "D" {
+				for i := 0; i < count; i++ {
+					y++
+					setLine(nLine, &field, x, y)
+				}
+			}
+			if arg == "U" {
+				for i := 0; i < count; i++ {
+					y--
+					setLine(nLine, &field, x, y)
+				}
+			}
+
 		}
 
 	}
@@ -39,6 +94,7 @@ func main() {
 	// 	initNumbers[i], _ = strconv.Atoi(s)
 	// }
 
-	fmt.Println("end")
+	// print2D(&field)
+	fmt.Println(findSmallestDist(&field, middle, middle))
 
 }
