cd2df2108a39add0311183d3e5e564d3961870d4
Alex Kalmakov <st00nsa@gmail.com>
2019-12-03 21:18:05 +0200

    So bad but works: day three - start two

--- 3/main.go
+++ 3/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"math"
 	"strconv"
 	"strings"
 )
@@ -14,25 +13,36 @@ func check(e error) {
 	}
 }
 
-func print2D(a *[30000][30000]int) {
+func print2D(a *[26000][26000][]int) {
 	for _, line := range a {
 		fmt.Println(line)
 	}
 }
 
-func setLine(nLine int, a *[30000][30000]int, x int, y int) {
-	if a[x][y] < nLine {
-		a[x][y] += nLine
+func setLine(nLine int, a *[26000][26000][]int, x int, y int, aCache map[string]bool, distance int) {
+	cacheKey := string(nLine) + ":" + string(x) + "," + string(y)
+	if !aCache[cacheKey] {
+
+		if a[x][y] == nil {
+			z := make([]int, 0)
+			a[x][y] = z
+		}
+
+		a[x][y] = append(a[x][y], distance)
+		aCache[cacheKey] = true
 	}
+
 }
 
-func findSmallestDist(a *[30000][30000]int, centerX int, centerY int) int {
-	smallestDistance := 0.0
-	for y, lineX := range a {
-		for x, value := range lineX {
-			if value == 3 {
-				dist := math.Sqrt(math.Pow(float64(x-centerX), 2)) + math.Sqrt(math.Pow(float64(y-centerY), 2))
-				if smallestDistance == 0 || dist < smallestDistance {
+func findSmallestDist(a *[26000][26000][]int, centerX int, centerY int) int {
+	smallestDistance := -1
+	for _, lineX := range a {
+		for _, value := range lineX {
+			// fmt.Println(value)
+			if len(value) == 2 {
+				// fmt.Println(value)
+				dist := value[0] + value[1]
+				if smallestDistance == -1 || dist < smallestDistance {
 					smallestDistance = dist
 				}
 			}
@@ -46,40 +56,46 @@ func main() {
 	check(err)
 	input := string(inputBytes)
 	lines := strings.Split(input, "\n")
-	len := 30000
+	len := 26000
 	middle := len / 2
-	field := [30000][30000]int{}
+	field := [26000][26000][]int{}
+	filedCache := make(map[string]bool)
 
 	for nLine, line := range lines {
 		nLine++
 		commands := strings.Split(line, ",")
 		x := middle
 		y := middle
+		distance := 0
 		for _, command := range commands {
 			arg := command[:1]
 			count, _ := strconv.Atoi(command[1:])
 			if arg == "R" {
 				for i := 0; i < count; i++ {
 					x++
-					setLine(nLine, &field, x, y)
+					distance++
+					setLine(nLine, &field, x, y, filedCache, distance)
 				}
 			}
 			if arg == "L" {
 				for i := 0; i < count; i++ {
 					x--
-					setLine(nLine, &field, x, y)
+					distance++
+					setLine(nLine, &field, x, y, filedCache, distance)
 				}
 			}
 			if arg == "D" {
 				for i := 0; i < count; i++ {
 					y++
-					setLine(nLine, &field, x, y)
+					distance++
+					setLine(nLine, &field, x, y, filedCache, distance)
 				}
 			}
 			if arg == "U" {
 				for i := 0; i < count; i++ {
 					y--
-					setLine(nLine, &field, x, y)
+					distance++
+					setLine(nLine, &field, x, y, filedCache, distance)
 				}
 			}
 
